8142d0c14e9df0de3004e9327c0dff19d2d6af74
calfonsi <calfonsi@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-09-01 14:44:16 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@6715 62dfeb86-a93f-4914-9d13-15c0a160985d

--- tao/install/update/1.3/17_activity_control.php
+++
@@ -1,9 +0,0 @@
-<?php
-$controlProperty = new core_kernel_classes_Property('http://www.tao.lu/middleware/taoqual.rdf#ActivityControl');
-
-$activityClazz = new core_kernel_classes_Class('http://www.tao.lu/middleware/taoqual.rdf#i118588757437650');
-foreach($activityClazz->getInstances(true) as $instance){
-	$instance->setPropertyValue($controlProperty, 'http://www.tao.lu/middleware/taoqual.rdf#ForwardControl');
-	$instance->setPropertyValue($controlProperty, 'http://www.tao.lu/middleware/taoqual.rdf#BackwardControl');
-}
-?>
