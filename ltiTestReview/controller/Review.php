1348eacf4dc908d30c654e3faa6cb5273a69d929
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2019-07-05 00:30:57 +0300

    Added controller for getting delivery execution for a review via LTI call

---
+++ ltiTestReview/controller/Review.php
@@ -0,0 +1,66 @@
+<?php
+/**
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; under version 2
+* of the License (non-upgradable).
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+* Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+*
+*
+*/
+
+namespace oat\taoReview\controller;
+
+use common_exception_Error;
+use oat\ltiDeliveryProvider\model\LtiLaunchDataService;
+use oat\ltiDeliveryProvider\model\LtiResultAliasStorage;
+use oat\taoLti\models\classes\LtiService;
+use tao_actions_SinglePageModule;
+
+/**
+ * Class Review
+ * @package oat\taoReview\controller
+ */
+class Review extends tao_actions_SinglePageModule
+{
+    /**
+     * @throws \oat\taoLti\models\classes\LtiException
+     * @throws \oat\taoLti\models\classes\LtiVariableMissingException
+     * @throws common_exception_Error
+     */
+    public function index()
+    {
+        $launchData = LtiService::singleton()->getLtiSession()->getLaunchData();
+
+        /** @var LtiLaunchDataService $launchDataService */
+        $launchDataService = $this->getServiceLocator()->get(LtiLaunchDataService::SERVICE_ID);
+
+        /** @var LtiResultAliasStorage $ltiResultIdStorage */
+        $ltiResultIdStorage = $this->getServiceLocator()->get(LtiResultAliasStorage::SERVICE_ID);
+
+
+        $resultIdentifier = $launchData->hasVariable('lis_result_sourcedid')
+            ? $launchData->getVariable('lis_result_sourcedid')
+            : $launchDataService->findDeliveryExecutionFromLaunchData($launchData);
+
+        $deliveryExecutionId = $ltiResultIdStorage->getDeliveryExecutionId($resultIdentifier);
+
+        if ($deliveryExecutionId !== null) {
+            $params['delivery_execution'] = $deliveryExecutionId;
+        }
+
+        $this->setClientRoute(_url('index', 'Review', 'taoReview', $params ?? []));
+        $this->composeView('delegated-view', null, 'pages/index.tpl', 'tao');
+    }
+
+}
