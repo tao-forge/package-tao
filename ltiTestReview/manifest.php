375121dd4afb2e9f425a7b4efa7dc01971ea43eb
Ricardo <ricardo@taotesting.com>
2019-08-08 15:38:00 +0200

    bump version to 0.9.0

--- ltiTestReview/manifest.php
+++ ltiTestReview/manifest.php
@@ -29,7 +29,7 @@ return [
     'label' => 'Review',
     'description' => 'Extension for reviewing passed tests, with the display of actual and correct answers.',
     'license' => 'GPL-2.0',
-    'version' => '0.8.0',
+    'version' => '0.9.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao' => '>=37.8.2',
