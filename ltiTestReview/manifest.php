31d10541305939cc5f90c069a53ead0e538fead5
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2019-09-09 17:12:58 +0300

    Bump version

--- ltiTestReview/manifest.php
+++ ltiTestReview/manifest.php
@@ -29,7 +29,7 @@ return [
     'label' => 'Review',
     'description' => 'Extension for reviewing passed tests, with the display of actual and correct answers.',
     'license' => 'GPL-2.0',
-    'version' => '1.10.0',
+    'version' => '1.10.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao' => '>=38.6.0',
