5f25f943f7a5dd5d1ac62f624f4d65097f671d22
Aleksej Tikhanovich <aleksej@taotesting.com>
2020-12-28 19:24:51 +0300

    refactor: bump version

--- ltiTestReview/manifest.php
+++ ltiTestReview/manifest.php
@@ -29,7 +29,7 @@ return [
     'label' => 'Test Review',
     'description' => 'Extension for reviewing passed tests, with the display of actual and correct answers.',
     'license' => 'GPL-2.0',
-    'version' => '1.17.0',
+    'version' => '1.17.0.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao' => '>=38.6.0',
